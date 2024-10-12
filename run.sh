b80a2f7ee65fd0b2a2aba314a9d44235767f4586
Doomshade <kuba.smrha@gmail.com>
2024-10-12 23:08:47 +0200

    Implement reading for most critical chunks

---
+++ run.sh
@@ -0,0 +1,1 @@
+cargo run -- ./resources/pngs/valid/minimal.png
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::env;
 use std::fmt;
 use std::fs;
-use std::io::{self, Error, ErrorKind};
+use std::io;
 
-const PNG_SIGNATURE: [u8; 8] = [0x89, b'P', b'N', b'G', 0x0D, 0x0A, 0x1A, 0x0A];
+const PNG_SIGNATURE_LENGTH: usize = 8;
+const PNG_SIGNATURE: [u8; PNG_SIGNATURE_LENGTH] = [0x89, b'P', b'N', b'G', 0x0D, 0x0A, 0x1A, 0x0A];
 
 #[derive(Debug, PartialEq, Eq)]
 enum ChunkType {
     Ihdr,
     Idat,
     Iend,
-    Eof,
-    Invalid,
 }
 
 impl fmt::Display for ChunkType {
@@ -20,44 +19,339 @@ impl fmt::Display for ChunkType {
             ChunkType::Ihdr => write!(f, "IHDR")?,
             ChunkType::Idat => write!(f, "IDAT")?,
             ChunkType::Iend => write!(f, "IEND")?,
-            ChunkType::Eof => write!(f, "EOF")?,
-            ChunkType::Invalid => write!(f, "Invalid")?,
         }
         Ok(())
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Chunk {
-    length: usize,
-    chunk_type: ChunkType,
-    data: Vec<u8>,
-    crc: [u8; 4],
+#[derive(Debug)]
+enum ChunkData {
+    Ihdr(IhdrChunkData),
+    Idat(Vec<u8>),
+    Iend,
 }
 
-impl Chunk {
-    pub fn new(length: usize, chunk_type: ChunkType, data: Vec<u8>, crc: [u8; 4]) -> Self {
-        Self {
-            length,
-            chunk_type,
-            data,
-            crc,
+#[derive(Debug, PartialEq)]
+enum BitDepth {
+    _1,
+    _2,
+    _4,
+    _8,
+    _16,
+}
+
+impl TryFrom<u8> for BitDepth {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            1 => Ok(BitDepth::_1),
+            2 => Ok(BitDepth::_2),
+            4 => Ok(BitDepth::_4),
+            8 => Ok(BitDepth::_8),
+            16 => Ok(BitDepth::_16),
+            v => Err(format!("Invalid bit depth: {v}")),
         }
     }
 }
 
-impl From<[u8; 4]> for ChunkType {
-    fn from(value: [u8; 4]) -> Self {
-        let s = match std::str::from_utf8(&value) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid shit: {}", e),
+impl BitDepth {
+    fn value(&self) -> u8 {
+        match &self {
+            BitDepth::_1 => 1,
+            BitDepth::_2 => 2,
+            BitDepth::_4 => 4,
+            BitDepth::_8 => 8,
+            BitDepth::_16 => 16,
+        }
+    }
+}
+
+impl fmt::Display for BitDepth {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ColorType {
+    Grayscale,
+    Truecolor,
+    Indexed,
+    GrayscaleAndAlpha,
+    TruecolorAndAlpha,
+}
+
+impl TryFrom<u8> for ColorType {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ColorType::Grayscale),
+            2 => Ok(ColorType::Truecolor),
+            3 => Ok(ColorType::Indexed),
+            4 => Ok(ColorType::GrayscaleAndAlpha),
+            6 => Ok(ColorType::TruecolorAndAlpha),
+            v => Err(format!("Invalid color type: {v}")),
+        }
+    }
+}
+
+impl ColorType {
+    fn value(&self) -> u8 {
+        match &self {
+            ColorType::Grayscale => 0,
+            ColorType::Truecolor => 2,
+            ColorType::Indexed => 3,
+            ColorType::GrayscaleAndAlpha => 4,
+            ColorType::TruecolorAndAlpha => 6,
+        }
+    }
+
+    fn combination_allowed(&self, bit_depth: &BitDepth) -> bool {
+        match &self {
+            ColorType::Grayscale => {
+                *bit_depth == BitDepth::_1
+                    || *bit_depth == BitDepth::_2
+                    || *bit_depth == BitDepth::_4
+                    || *bit_depth == BitDepth::_8
+                    || *bit_depth == BitDepth::_16
+            }
+            ColorType::Truecolor => *bit_depth == BitDepth::_8 || *bit_depth == BitDepth::_16,
+            ColorType::Indexed => {
+                *bit_depth == BitDepth::_1
+                    || *bit_depth == BitDepth::_2
+                    || *bit_depth == BitDepth::_4
+                    || *bit_depth == BitDepth::_8
+            }
+            ColorType::GrayscaleAndAlpha => {
+                *bit_depth == BitDepth::_8 || *bit_depth == BitDepth::_16
+            }
+            ColorType::TruecolorAndAlpha => {
+                *bit_depth == BitDepth::_8 || *bit_depth == BitDepth::_16
+            }
+        }
+    }
+}
+
+impl fmt::Display for ColorType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str_representation = match self {
+            ColorType::Grayscale => "grayscale",
+            ColorType::Truecolor => "truecolor",
+            ColorType::Indexed => "indexed",
+            ColorType::GrayscaleAndAlpha => "grayscale and alpha",
+            ColorType::TruecolorAndAlpha => "truecolor and alpha",
         };
+        write!(f, "{} ({})", self.value(), str_representation)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum CompressionMethod {
+    DeflateInflate,
+}
+
+impl TryFrom<u8> for CompressionMethod {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(CompressionMethod::DeflateInflate),
+            v => Err(format!("Unknown compression method: {v}")),
+        }
+    }
+}
+
+impl CompressionMethod {
+    fn value(&self) -> u8 {
+        match self {
+            CompressionMethod::DeflateInflate => 0,
+        }
+    }
+}
+
+impl fmt::Display for CompressionMethod {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str_representation = match self {
+            CompressionMethod::DeflateInflate => "deflate/inflate",
+        };
+        write!(f, "{} ({})", self.value(), str_representation)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum FilterMethod {
+    AdaptiveFiltering,
+}
+
+impl TryFrom<u8> for FilterMethod {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(FilterMethod::AdaptiveFiltering),
+            v => Err(format!("Unknown filter method: {v}")),
+        }
+    }
+}
+
+impl FilterMethod {
+    fn value(&self) -> u8 {
+        match self {
+            FilterMethod::AdaptiveFiltering => 0,
+        }
+    }
+}
+
+impl fmt::Display for FilterMethod {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str_representation = match self {
+            FilterMethod::AdaptiveFiltering => "adaptive filtering with five basic filter types",
+        };
+        write!(f, "{} ({})", self.value(), str_representation)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum InterlaceMethod {
+    None,
+    Adam7,
+}
+
+impl TryFrom<u8> for InterlaceMethod {
+    type Error = String;
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(InterlaceMethod::None),
+            1 => Ok(InterlaceMethod::Adam7),
+            v => Err(format!("Unknown interlace method: {v}")),
+        }
+    }
+}
+
+impl InterlaceMethod {
+    fn value(&self) -> u8 {
+        match self {
+            InterlaceMethod::None => 0,
+            InterlaceMethod::Adam7 => 1,
+        }
+    }
+}
+
+impl fmt::Display for InterlaceMethod {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let str_representation = match self {
+            InterlaceMethod::None => "no interlace",
+            InterlaceMethod::Adam7 => "Adam7 interlace",
+        };
+        write!(f, "{} ({})", self.value(), str_representation)
+    }
+}
+
+#[derive(Debug)]
+struct IhdrChunkData {
+    width: u32,
+    height: u32,
+    bit_depth: BitDepth,
+    color_type: ColorType,
+    compression_method: CompressionMethod,
+    filter_method: FilterMethod,
+    interlace_method: InterlaceMethod,
+}
+
+impl TryFrom<Vec<u8>> for IhdrChunkData {
+    type Error = String;
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        let mut buf = [0; 4];
+        let mut iter = value.iter();
+
+        for i in 0..4 {
+            buf[i] = *iter.next().ok_or_else(|| "Expected 4 bytes for width")?;
+        }
+
+        let width = u32::from_be_bytes(buf);
+
+        buf = [0; 4];
+
+        for i in 0..4 {
+            buf[i] = *iter.next().ok_or_else(|| "Expected 4 bytes for height")?;
+        }
+
+        let height = u32::from_be_bytes(buf);
+
+        let bit_depth = BitDepth::try_from(*iter.next().ok_or_else(|| "Expected bit depth byte")?)?;
+
+        let color_type =
+            ColorType::try_from(*iter.next().ok_or_else(|| "Expected color type byte")?)?;
+
+        if !color_type.combination_allowed(&bit_depth) {
+            return Err(format!("The combination of bit depth {bit_depth} and color type {color_type} is not allowed"));
+        }
+
+        let compression_method = CompressionMethod::try_from(
+            *iter
+                .next()
+                .ok_or_else(|| "Expected compression method byte")?,
+        )?;
+
+        let filter_method =
+            FilterMethod::try_from(*iter.next().ok_or_else(|| "Expected filter method byte")?)?;
+
+        let interlace_method = InterlaceMethod::try_from(
+            *iter
+                .next()
+                .ok_or_else(|| "Expected interlace method byte")?,
+        )?;
+
+        Ok(Self {
+            width,
+            height,
+            bit_depth,
+            color_type,
+            compression_method,
+            filter_method,
+            interlace_method,
+        })
+    }
+}
+
+#[derive(Debug)]
+struct Chunk {
+    length: usize,
+    data: ChunkData,
+    crc: [u8; 4],
+}
+
+impl Chunk {
+    pub fn new(length: usize, data: ChunkData, crc: [u8; 4]) -> Self {
+        Self { length, data, crc }
+    }
+    fn length(&self) -> usize {
+        self.length
+    }
+    fn crc(&self) -> &[u8; 4] {
+        &self.crc
+    }
+    fn data(&self) -> &ChunkData {
+        &self.data
+    }
+    fn chunk_type(&self) -> &str {
+        match self.data {
+            ChunkData::Ihdr(_) => "IHDR",
+            ChunkData::Idat(_) => "IDAT",
+            ChunkData::Iend => "IEND",
+            _ => "Unknown chunk type",
+        }
+    }
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    type Error = String;
+    fn try_from(value: [u8; 4]) -> Result<Self, Self::Error> {
+        let s = std::str::from_utf8(&value).map_err(|err| err.to_string())?;
 
         match s {
-            "IHDR" => ChunkType::Ihdr,
-            "IDAT" => ChunkType::Idat,
-            "IEND" => ChunkType::Iend,
-            _ => ChunkType::Invalid,
+            "IHDR" => Ok(ChunkType::Ihdr),
+            "IDAT" => Ok(ChunkType::Idat),
+            "IEND" => Ok(ChunkType::Iend),
+            _ => Err(format!("Invalid chunk type: {s}")),
         }
     }
 }
@@ -72,13 +366,15 @@ impl<I: ExactSizeIterator<Item = u8>> PngIterator<I> {
     }
 
     fn validate_png_signature(&mut self) -> io::Result<()> {
-        for &expected in &PNG_SIGNATURE {
-            match self.inner.next() {
-                Some(byte) if byte == expected => continue,
-                _ => return Err(Error::new(ErrorKind::InvalidData, "Invalid PNG signature")),
-            }
+        let png_signature = self.read_bytes::<PNG_SIGNATURE_LENGTH>()?;
+        if png_signature == PNG_SIGNATURE {
+            return Ok(());
+        } else {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Invalid PNG signature",
+            ));
         }
-        Ok(())
     }
 
     /// The chunk consists of the following parts:
@@ -87,16 +383,15 @@ impl<I: ExactSizeIterator<Item = u8>> PngIterator<I> {
     /// - chunk data (length bytes)
     /// - CRC (4 bytes)
     fn parse_chunk(&mut self) -> io::Result<Chunk> {
-        if self.inner.len() == 0 {
-            return Ok(Chunk::new(0, ChunkType::Eof, vec![], [0; 4]));
-        }
-
         let length = u32::from_be_bytes(self.read_bytes()?);
-        let chunk_type = ChunkType::from(self.read_bytes()?);
-        let mut chunk_data: Vec<u8> = Vec::with_capacity(length as usize);
+        let chunk_type = ChunkType::try_from(self.read_bytes()?)
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+        let mut chunk_data_raw: Vec<u8> = Vec::with_capacity(length as usize);
+
+        println!("Reading {length} bytes (chunk data)");
 
         for _ in 0..length {
-            chunk_data.push(self.inner.next().ok_or_else(|| {
+            chunk_data_raw.push(self.inner.next().ok_or_else(|| {
                 io::Error::new(
                     io::ErrorKind::UnexpectedEof,
                     "Failed to read the whole chunk",
@@ -105,8 +400,14 @@ impl<I: ExactSizeIterator<Item = u8>> PngIterator<I> {
         }
 
         let crc = self.read_bytes()?;
+        let chunk_data = match chunk_type {
+            ChunkType::Ihdr => ChunkData::Ihdr(IhdrChunkData::try_from(chunk_data_raw).unwrap()),
+            ChunkType::Idat => ChunkData::Idat(chunk_data_raw),
+            ChunkType::Iend => ChunkData::Iend,
+            _ => todo!(),
+        };
 
-        Ok(Chunk::new(length as usize, chunk_type, chunk_data, crc))
+        Ok(Chunk::new(length as usize, chunk_data, crc))
     }
 
     fn read_bytes<const N: usize>(&mut self) -> io::Result<[u8; N]> {
@@ -114,6 +415,8 @@ impl<I: ExactSizeIterator<Item = u8>> PngIterator<I> {
         if N == 0 {
             return Ok(buf);
         }
+
+        println!("Reading {N} bytes");
 
         for b in &mut buf {
             *b = self
@@ -129,24 +432,45 @@ impl<I: ExactSizeIterator<Item = u8>> PngIterator<I> {
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let args: Vec<String> = env::args().collect();
     let file = args.get(1).expect("Please provide a path to the PNG file");
-    let contents = fs::read(file)?;
+    let png_file = parse_png(file)?;
+    dbg!(png_file);
+    Ok(())
+}
+
+#[derive(Debug)]
+struct PngFile {
+    chunks: Vec<Chunk>,
+}
+
+fn parse_png(file_path: &str) -> Result<PngFile, Box<dyn std::error::Error + 'static>> {
+    let mut chunks = vec![];
+    let contents = fs::read(file_path)?;
     let mut png_iter = PngIterator::new(contents.into_iter());
     png_iter.validate_png_signature()?;
     println!("PNG signature valid");
     loop {
         let chunk = png_iter.parse_chunk()?;
-        if chunk.chunk_type == ChunkType::Eof {
+        println!("Chunk type: {}", chunk.chunk_type());
+        if chunk.chunk_type() == "IEND" {
+            chunks.push(chunk);
             println!("Read all chunks");
-            return Ok(());
+            return Ok(PngFile { chunks });
         }
-
-        println!("Length: {}", &chunk.length);
-        println!("Chunk type: {}", &chunk.chunk_type);
-        println!("Data: {:?}", &chunk.data);
+        chunks.push(chunk);
     }
 }
 
 #[test]
-fn test_valid_pngs() {
-    todo!();
+fn test_valid_pngs() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let png_dir = fs::read_dir("resources/pngs/valid")?;
+
+    for png in png_dir {
+        let p = parse_png(
+            png?.path()
+                .to_str()
+                .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Path not found"))?,
+        )?;
+        dbg!(p);
+    }
+    Ok(())
 }
